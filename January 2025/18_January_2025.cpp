cf650bf3cd251c3181864a352580d75f6fe566e2
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-18 22:03:39 +0530

    Day 18 done

---
+++ January 2025/18_January_2025.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    Node* reverseList(Node* head) {
+        Node *prev = NULL, *curr = head, *next;
+        while (curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
